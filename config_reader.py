5f3c8c021e58831849c049cc894ceee8dd1538c1
Barak <barakcloud@hotmail.com>
2021-04-13 15:13:09 +0300

    change in config reader

--- config_reader.py
+++ config_reader.py
@@ -1,7 +1,9 @@
 import os
 import json
+import pathlib
 
-config_dir = os.path.join(__file__,'..')
+config_dir= pathlib.Path().absolute()
+#config_dir = os.path.join(__file__,'..')
 with open(os.path.join(config_dir,'config.json'), 'rt' ) as f:
     data = json.load(f)
 
